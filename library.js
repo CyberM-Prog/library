cb0590a8aa46942b56c53974ff9d7523894cfaee
Martim <martim.pintao@gmail.com>
2022-06-08 11:09:36 +0100

    Create function that create new rows on the table for each book

--- library.js
+++ library.js
@@ -13,3 +13,27 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(book) {
     myLibrary.push(book)
 }
+
+function displayBook() {
+    myLibrary.forEach(book => {
+
+        const tableRow = document.createElement("tr");
+        const tableHeaderTitle = document.createElement("td");
+        const tableHeaderAuthor = document.createElement("td");
+        const tableHeaderPages = document.createElement("td");
+        const tableHeaderRead = document.createElement("td");
+
+        tableHeaderTitle.textContent = book.title
+        tableHeaderAuthor.textContent = book.author
+        tableHeaderPages.textContent = book.pages
+        tableHeaderRead.textContent = book.read
+
+        const tbody = document.querySelector("tbody")
+        tbody.appendChild(tableRow)
+        
+        tableRow.appendChild(tableHeaderTitle)
+        tableRow.appendChild(tableHeaderAuthor)
+        tableRow.appendChild(tableHeaderPages)
+        tableRow.appendChild(tableHeaderRead)
+    });
+}
