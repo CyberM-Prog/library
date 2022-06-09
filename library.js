4f0114bcd404b4ca2fa234f2df904226fe92e8a3
Martim <martim.pintao@gmail.com>
2022-06-09 18:13:08 +0100

    Create button that removes books from the library

--- library.js
+++ library.js
@@ -14,27 +14,58 @@ function addBookToLibrary(book) {
     myLibrary.push(book)
 }
 
-function displayBook(book) {
+let displayBookCounter = (function() {
+    
+    let n
+
+    return function displayBook(book) {	
+
+        if (!n) {n = 0}
+
+        const removeButton = document.createElement("button")
         const tableRow = document.createElement("tr");
         const tableHeaderTitle = document.createElement("td");
         const tableHeaderAuthor = document.createElement("td");
         const tableHeaderPages = document.createElement("td");
         const tableHeaderRead = document.createElement("td");
+        const tableRemove = document.createElement("td");
+
+        removeButton.setAttribute("data-book", n)
+        removeButton.setAttribute("class", "remove")
+        tableRow.setAttribute("data-book", n)
+        tableHeaderTitle.setAttribute("data-book", n)
+        tableHeaderAuthor.setAttribute("data-book", n)
+        tableHeaderPages.setAttribute("data-book", n)
+        tableHeaderRead.setAttribute("data-book", n)
+        tableRemove.setAttribute("data-book", n)
+        n++
 
         tableHeaderTitle.textContent = book.title
         tableHeaderAuthor.textContent = book.author
         tableHeaderPages.textContent = book.pages
         tableHeaderRead.textContent = book.read
+        removeButton.textContent = "Remove"
 
-        const tbody = document.querySelector("tbody")
-        tbody.appendChild(tableRow)
+        const table = document.querySelector("table")
+        table.appendChild(tableRow)
         
         tableRow.appendChild(tableHeaderTitle)
         tableRow.appendChild(tableHeaderAuthor)
         tableRow.appendChild(tableHeaderPages)
         tableRow.appendChild(tableHeaderRead)
-  
-}
+        tableRow.appendChild(tableRemove)
+
+        tableRemove.appendChild(removeButton)
+
+        removeButton.addEventListener("click", function() {
+
+        
+            let dataIndex = removeButton.getAttribute("data-book")
+            delete myLibrary[dataIndex]
+            tableRow.parentElement.removeChild(tableRow)        
+        })
+    };
+})();
 
 const newBookButton = document.querySelector(".newbtn")
 const popUp = document.querySelector(".darkbg")
@@ -61,7 +92,7 @@ function addBookToDisplay() {
     let newBook = new Book(title.value, author.value, pages.value, haveRead)
     
     addBookToLibrary(newBook)
-    displayBook(newBook)
+    displayBookCounter(newBook)
 
     popUp.classList.add("hide")
 
